92337a645993e9312973e57ddb56ab0a543820ff
Alexander Foremny <aforemny@posteo.de>
2018-08-05 21:57:50 +0200

    Add Mwc.Typography

---
+++ src/Mwc/Typography.elm
@@ -0,0 +1,59 @@
+module Mwc.Typography exposing (..)
+
+import Html.Attributes as Html
+
+
+typography =
+    Html.class "mdc-typography"
+
+
+headline1 =
+    Html.class "mdc-typography--headline1"
+
+
+headline2 =
+    Html.class "mdc-typography--headline2"
+
+
+headline3 =
+    Html.class "mdc-typography--headline3"
+
+
+headline4 =
+    Html.class "mdc-typography--headline4"
+
+
+headline5 =
+    Html.class "mdc-typography--headline5"
+
+
+headline6 =
+    Html.class "mdc-typography--headline6"
+
+
+subtitle1 =
+    Html.class "mdc-typography--subtitle1"
+
+
+subtitle2 =
+    Html.class "mdc-typography--subtitle2"
+
+
+body1 =
+    Html.class "mdc-typography--body1"
+
+
+body2 =
+    Html.class "mdc-typography--body2"
+
+
+caption =
+    Html.class "mdc-typography--caption"
+
+
+button =
+    Html.class "mdc-typography--button"
+
+
+overline =
+    Html.class "mdc-typography--overline"
